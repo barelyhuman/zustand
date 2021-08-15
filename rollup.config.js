2cdb8ac318621b4e48c6dc596d9f26626329467b
Daishi Kato <dai-shi@users.noreply.github.com>
2021-08-16 07:16:17 +0900

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -70,11 +70,13 @@ export default function (args) {
     return [
       createCommonJSConfig(`src/${c}.ts`, `dist/${c}.js`),
       createESMConfig(`src/${c}.ts`, `dist/esm/${c}.js`),
+      createESMConfig(`src/${c}.ts`, `dist/esm/${c}.mjs`),
     ]
   }
   return [
     createDeclarationConfig('src/index.ts', 'dist'),
     createCommonJSConfig('src/index.ts', 'dist/index.js'),
     createESMConfig('src/index.ts', 'dist/esm/index.js'),
+    createESMConfig('src/index.ts', 'dist/esm/index.mjs'),
   ]
 }
