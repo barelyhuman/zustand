129a9b749611a7ee8068d7be3fa001e93498a500
Siddharth <ahoy@barelyhuman.dev>
2021-08-26 14:34:42 +0530

    Additional handling for keys

--- scripts/create-esm-wrapper.js
+++ scripts/create-esm-wrapper.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 const { writeFileSync, mkdirSync, existsSync } = require('fs')
-const { resolve } = require('path')
+const { resolve, dirname } = require('path')
 
 function main() {
   const [sourceFile] = process.argv.slice(2)
@@ -11,33 +11,38 @@ function main() {
   }
 
   let normalizedFile = sourceFile.replace('../', '').replace('./', '')
-  const cjsMod = require(resolve(sourceFile))
-  const keys = new Set(Object.getOwnPropertyNames(cjsMod))
+  const _cjsModule = require(resolve(sourceFile))
+  const keys = new Set(Object.getOwnPropertyNames(_cjsModule))
   keys.delete('__esModule')
+
+  if (typeof _cjsModule === 'function') {
+    for (const key of ['length', 'prototype', 'name', 'caller']) {
+      keys.delete(key)
+    }
+  }
 
   let outputString = `// esm-wrapper
   export * from '../${normalizedFile}';
+  import _module from '../${normalizedFile}';
   `
 
   let hasDefault = false
   for (const key of [...keys].sort()) {
     if (key === 'default') {
       hasDefault = true
-      outputString += `import mod from '../${normalizedFile}';\nexport default mod.default;`
+      outputString += `export default _module.default;`
       continue
     }
-    outputString += `export const ${key} = mod.${key};\n`
+    outputString += `export const ${key} = _module.${key};\n`
   }
 
   if (!hasDefault) {
-    outputString += `export { default } from '../${normalizedFile}'`
+    outputString += `export default _module`
   }
 
-  if (!existsSync('esm')) {
-    mkdirSync('esm')
-  }
-
-  writeFile(`esm/${sourceFile}`, outputString)
+  const targetFile = `esm/${sourceFile}`
+  mkdirSync(dirname(targetFile), { recursive: true })
+  writeFile(targetFile, outputString)
 }
 
 function writeFile(path, fileData) {
