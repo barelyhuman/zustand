a6135787528ae4a50009cb31e3382003cbe5efe7
reaper <ahoy@barelyhuman.dev>
2023-02-25 20:40:29 +0530

    fix: reorder module.exports

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -4,6 +4,7 @@ const { resolve, basename } = require('path')
 const glob = require('tiny-glob')
 
 async function fixExports() {
+  const exportDefRgx = /^exports\.(\w+)\s+=\s+(\w+)[;]?/gm
   const rootDir = resolve('.')
   const files = await glob('*.js', {
     absolute: true,
@@ -12,7 +13,24 @@ async function fixExports() {
   for (const file of files) {
     console.log(`>> Fix ${basename(file)}`)
     let code = await fs.readFile(file, 'utf8')
-    code = code.replace('exports.default =', 'module.exports =')
+    const exportContexts = []
+    const exportDefs = code.matchAll(exportDefRgx)
+    for (const exportStmt of exportDefs) {
+      exportContexts.push([exportStmt[1], exportStmt[2]])
+    }
+    const defIndex = exportContexts.indexOf('default')
+    if (defIndex > -1) {
+      exportContexts.splice(defIndex, 1)
+      exportContexts.unshift('default')
+    }
+
+    exportContexts.forEach((ctx) => {
+      if (ctx === 'default') {
+        code += 'module.exports = exports.default;'
+      } else {
+        code += `module.exports.${ctx[0]} = ${ctx[1]};`
+      }
+    })
     code += 'exports.default = module.exports;'
     await fs.writeFile(file, code)
   }
